d30fdf26de403851b2e18e9254b97f2da851517a
Jean <81425194+jenchen-dev@users.noreply.github.com>
2022-06-04 12:20:15 +0800

    add checkout total amount calculation and empty cart message

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,11 +3,12 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CartImage from '../../assets/shopping-cart.png';
 
 import './checkout.styles.scss';
 
 const CheckOut = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, cartTotal } = useContext(CartContext);
 
     return(
         <div className='checkout-container'>
@@ -29,11 +30,19 @@ const CheckOut = () => {
                 </div>
             </div>
             {
-                cartItems.map((item) => (
-                    <CheckoutItem key={item.id} cartItem={item} />
-                ))
+                cartItems.length > 0 ? (
+                    cartItems.map((item) => (
+                        <CheckoutItem key={item.id} cartItem={item} />
+                    ))
+                ) : (
+                    <div className='empty-message'>
+                        <img src={CartImage} alt="shopping-cart" height={50} width={50} />
+                        <h3>Your cart is empty</h3>
+                        <p>start adding something that makes you look fashion</p>
+                    </div>
+                )
             }
-            <span className='total'>Total: 0</span>
+            <span className='total'>Total: $ {cartTotal}</span>
         </div>
     );
 };
